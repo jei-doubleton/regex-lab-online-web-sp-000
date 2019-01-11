4b79701f2d3cf7f9563a7a26394cca4fcdeb711c
J <jennifer.singleton@gmail.com>
2019-01-11 12:50:40 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,9 @@
 def starts_with_a_vowel?(word)
-
+  if /\b[aeiou]/
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -7,7 +11,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
